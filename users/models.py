cd05b76443a47dfac7a4167fe5abc9ca259a1bda
ВашЛогин <Email>
2025-11-28 14:48:11 +0300

    Add custom User model with roles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        PM = 'PM', 'Project Manager'
+        DEV = 'DEV', 'Developer'
+
+    role = models.CharField(max_length=10, choices=Roles.choices, default=Roles.DEV)
+
+    def is_admin_role(self):
+        return self.role == self.Roles.ADMIN
+
+    def is_pm(self):
+        return self.role == self.Roles.PM
+
+    def is_dev(self):
+        return self.role == self.Roles.DEV
