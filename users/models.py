83bd681c0952d980d2d10d3b084f3fb6df8d6c26
ВашЛогин <Email>
2025-12-10 11:53:43 +0300

    translate models and atributes

--- users/models.py
+++ users/models.py
@@ -1,14 +1,44 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     class Roles(models.TextChoices):
         ADMIN = 'ADMIN', 'Admin'
-        PM = 'PM', 'Project Manager'
-        DEV = 'DEV', 'Developer'
+        PM = 'PM', 'Менеджер проекта'
+        DEV = 'DEV', 'Разработчик'
 
-    role = models.CharField(max_length=10, choices=Roles.choices, default=Roles.DEV)
+    # Удаляем first_name, last_name, вводим обязательное поле full_name
+    first_name = None
+    last_name = None
 
+    full_name = models.CharField(
+        max_length=255,
+        verbose_name="ФИО",
+        null=False,
+        blank=False,
+        default="",
+        help_text="Полное имя пользователя",
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=Roles.choices,
+        default=Roles.ADMIN,
+        verbose_name="Роль пользователя",
+    )
+
+    # Значения по умолчанию
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        # Админ всегда суперюзер
+        if self.role == self.Roles.ADMIN:
+            self.is_superuser = True
+        super().save(*args, **kwargs)
+
+    # Методы ролей
     def is_admin_role(self):
         return self.role == self.Roles.ADMIN
 
@@ -17,3 +47,10 @@ class User(AbstractUser):
 
     def is_dev(self):
         return self.role == self.Roles.DEV
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.full_name
