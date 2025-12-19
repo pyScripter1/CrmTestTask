366c105f0c770b1d0d1718202f100ba4e304de9d
ВашЛогин <Email>
2025-12-19 12:01:14 +0300

    adding urls for kanban

--- crm_system/urls.py
+++ crm_system/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for crm_system project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,15 +7,17 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # API endpoints
-    path('api/', include('crm.urls')),
+    # HTML
+    path('', include('crm.urls')),
+
+    # API
+    path('api/', include('crm.api_urls')),
     path('api/auth/', include('users.urls')),
 
-    # API documentation
+    # API docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
-# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
