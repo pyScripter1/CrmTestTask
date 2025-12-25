9640395ab23a85f367e9d8f8673f9f50fc8b12d1
ВашЛогин <Email>
2025-12-25 15:03:06 +0300

    adding new urls

--- crm/api_urls.py
+++ crm/api_urls.py
@@ -5,6 +5,8 @@ from .views import (
     kanban_task_update,
     kanban_task_delete,
     kanban_reorder,
+    kanban_task_history,
+    kanban_project_activity,
 )
 
 urlpatterns = [
@@ -13,4 +15,7 @@ urlpatterns = [
     path("kanban/task/<int:task_id>/", kanban_task_update),
     path("kanban/task/<int:task_id>/delete/", kanban_task_delete),
     path("kanban/reorder/", kanban_reorder),
+    path("kanban/task/<int:task_id>/history/", kanban_task_history),
+    path("kanban/project/<int:project_id>/activity/", kanban_project_activity),
+
 ]
